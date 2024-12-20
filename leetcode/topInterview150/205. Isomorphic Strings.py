8841853926834a9e2f9c9f221638f4b1b0f03e4c
SUKJIN HAN <0626na@gmail.com>
2024-12-20 18:36:24 +0900

    leetcode 205. Isomorphic Strings 푸는 중

---
+++ leetcode/topInterview150/205. Isomorphic Strings.py
@@ -0,0 +1,12 @@
+"""
+leetcode Top Interview 150 205. Isomorphic Strings. Hash Table, String
+
+Given two strings s and t, determine if they are isomorphic.
+Two strings s and t are isomorphic if the characters in s can be replaced to get t.
+All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character, but a character may map to itself.
+"""
+
+
+def isIsomorphic(s: str, t: str) -> bool:
+
+    return
