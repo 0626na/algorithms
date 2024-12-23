ee6277459067c52b09ec6df49053044c30a2349f
SUKJIN HAN <0626na@gmail.com>
2024-12-23 10:01:10 +0000

    leetcode Top Interview 150 202. Happy Number 문제 풀이중

---
+++ leetcode/topInterview150/202. Happy Number.py
@@ -0,0 +1,55 @@
+"""
+leetcode Top Interview 150 202. Happy Number. Hash Table, Math, Two Pointers
+
+Write an algorithm to determine if a number n is happy.
+
+A happy number is a number defined by the following process:
+
+Starting with any positive integer, replace the number by the sum of the squares of its digits.
+Repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1.
+Those numbers for which this process ends in 1 are happy.
+Return true if n is a happy number, and false if not.
+
+ 
+
+Example 1:
+
+Input: n = 19
+Output: true
+Explanation:
+12 + 92 = 82
+82 + 22 = 68
+62 + 82 = 100
+12 + 02 + 02 = 1
+
+Example 2:
+
+Input: n = 2
+Output: false
+ 
+
+Constraints:
+
+1 <= n <= 231 - 1
+
+"""
+
+
+def isHappy(n: int) -> bool:
+    sumOfSquares = n
+    hashT = {}
+
+    while sumOfSquares != 1:
+        digits = [int(s) ** 2 for s in str(sumOfSquares)]
+
+        for d in digits:
+            hashT[d] = 1
+        sumOfSquares = sum(digits)
+
+        if sumOfSquares in hashT:
+            return False
+
+    return True
+
+
+isHappy(19)
