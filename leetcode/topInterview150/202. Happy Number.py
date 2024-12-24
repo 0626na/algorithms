e4d7aee9e6e3c50f7f62095e123401dec45ce924
SUKJIN HAN <0626na@gmail.com>
2024-12-24 14:55:21 +0900

    leetcode Top Interview 150 202. Happy Number. 풀이완료

--- leetcode/topInterview150/202. Happy Number.py
+++ leetcode/topInterview150/202. Happy Number.py
@@ -34,22 +34,27 @@ Constraints:
 
 """
 
+"""
+풀이 방법
+- 1이 나오지 않는 cycle이 생기는지를 확인해야 한다. 여기서 cycle이란 계속 각 자리수를 제곱해서 더할때 이전에 나왔던 숫자가 또 반복해서 나오는 경우를 말한다.
+- 각 자리수 제곱의 합을 dictionary에 저장한다. 해당 값이 없으면 각 자리수의 제곱을 계속 이어가고 있다면 false를 반환한다.
+- 계산을 이어가다가 각 자리 제곱의 합이 1이 나오면 True를 반환한다.
+"""
+
 
 def isHappy(n: int) -> bool:
     sumOfSquares = n
     hashT = {}
 
     while sumOfSquares != 1:
-        digits = [int(s) ** 2 for s in str(sumOfSquares)]
-
-        for d in digits:
-            hashT[d] = 1
-        sumOfSquares = sum(digits)
-
         if sumOfSquares in hashT:
             return False
+        hashT[sumOfSquares] = 1
+        digits = [int(s) ** 2 for s in str(sumOfSquares)]
+
+        sumOfSquares = sum(digits)
 
     return True
 
 
-isHappy(19)
+isHappy(7)
