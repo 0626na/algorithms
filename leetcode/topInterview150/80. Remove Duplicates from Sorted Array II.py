00f06752cb6cb2bdd02a0bfd784a0bf60cdda4f4
SUKJIN HAN <0626na@gmail.com>
2025-01-14 19:16:30 +0900

    leetcode Top Interview 150 80. Remove Duplicates from Sorted Array II. 파이썬으로 풀이 완료 하지만 이 문제는 아직 제대로 이해하지 못했기에 다시 풀어볼 필요가 있음

--- leetcode/topInterview150/80. Remove Duplicates from Sorted Array II.py
+++ leetcode/topInterview150/80. Remove Duplicates from Sorted Array II.py
@@ -1,7 +1,8 @@
 """
-leetcode Top Interview 150 80. Remove Duplicates from Sorted Array II. Array, Two Pointers
+leetcode Top Interview 150 
+80. Remove Duplicates from Sorted Array II. Array, Two Pointers
 
-Given an integer array nums sorted in non-decreasing order, remove some duplicates in-place such that each unique element appears at most twice. The relative order of the elements should be kept the same.
+Given an integer array nums sovrted in non-decreasing order, remoe some duplicates in-place such that each unique element appears at most twice. The relative order of the elements should be kept the same.
 
 Since it is impossible to change the length of the array in some languages, you must instead have the result be placed in the first part of the array nums. More formally, if there are k elements after removing the duplicates, then the first k elements of nums should hold the final result. It does not matter what you leave beyond the first k elements.
 
@@ -46,3 +47,23 @@ Constraints:
 -104 <= nums[i] <= 104
 nums is sorted in non-decreasing order.
 """
+
+from typing import List
+
+
+def removeDuplicates(nums: List[int]) -> int:
+    if len(nums) < 3:
+        return len(nums)
+
+    k = 2
+    for i in range(2, len(nums)):
+        # 새 요소가 결과 배열 마지막 두 요소와 다르면 결과에 추가
+        if nums[i] != nums[k - 2]:
+            nums[k] = nums[i]
+            k += 1
+
+    return k
+
+
+removeDuplicates([0, 0, 1, 1, 1, 1, 2, 3, 3])
+#                 0  1  2  3  4  5  6  7  8
